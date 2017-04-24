4fb07d970129241956c7e2857332ebb20af069d8
Dave Falke <dmfalke@gmail.com>
2017-04-24 18:53:34 +0000

    merge from upstream trunk #25426
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/MicrobiomeWebsite/branches/r25426@81301 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/mainMenuItems.js
+++ Site/webapp/wdkCustomization/js/client/mainMenuItems.js
@@ -7,25 +7,6 @@ import {
 } from 'lodash';
 
 /**
- ** this is used below to stop the default menu items list -coming from eupathsitecommon (SiteHeader.jsx)- at favorites (excluding)
- **
- ** @return {boolean}
- **/
-function isFavorites(item) {
-  return item.id === 'favorites';
-}
-
-/**
- ** this is used below to stop the menu items list -coming from eupathsitecommon (SiteHeader.jsx)- at twitter (excluding)
- **
- ** @return {boolean}
- **/
-function isTwitter(item) {
-  return item.id === 'twitter';
-}
-
-
-/**
  ** Get subset of defaultItems we want to show in menu.
  **
  ** @return {Array<Item>}
@@ -37,27 +18,24 @@ function getInitialItems(defaultItems) {
 }
 
 /**
- ** Get favorites link menu item
- **
- ** @return {Item}
- **/
-function findFavoritesItem(defaultItems) {
-  return find(defaultItems, isFavorites);
-}
-
-/**
  ** Get menu items
  **
  ** @return {Array<Item>}
  **/
 export default function mainMenuItems({ siteConfig, preferences }, defaultItems) {
-  return getInitialItems(defaultItems).concat([
+  return [ 
+    defaultItems.search,
+    defaultItems.strategies,
+    defaultItems.basket,
+    defaultItems.favorites,
     { 
       id: 'datasets',
       text: 'Data Sets',
       route: 'search/dataset/AllDatasets/result'
     },
-    findFavoritesItem(defaultItems)
-  ]);
+   defaultItems.profileOrLogin,
+   defaultItems.registerOrLogout,
+   defaultItems.contactUs
+  ];
 }
 
