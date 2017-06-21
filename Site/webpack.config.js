2d2590b0fffcdcf66d7dda45d89fc9ee8ab3c019
Dave Falke <dmfalke@gmail.com>
2017-06-21 18:55:46 +0000

    streamline css bundling, and update cytoscape menu on pathway record page.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/MicrobiomeWebsite/trunk@82436 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webpack.config.js
+++ Site/webpack.config.js
@@ -2,10 +2,10 @@ var configure = require('../../EbrcWebsiteCommon/Site/site.webpack.config');
 
 module.exports = configure({
   entry: {
-    'legacy': [
+    'site-legacy': [
       './webapp/wdkCustomization/js/client/main.js',
       require.resolve('../../EbrcWebsiteCommon/Site/webapp/wdkCustomization/js/common.js')
     ],
-    'client': './webapp/wdkCustomization/js/client/main.js'
+    'site-client': './webapp/wdkCustomization/js/client/main.js'
   }
 });
