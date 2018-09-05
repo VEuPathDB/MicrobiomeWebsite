688e681c57ea8822c8badb9a27e72f19497f3a22
Danielle Callan <dcallan@upenn.edu>
2018-09-05 23:16:25 +0000

    prevent diff abun app from automatically populating ui
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/MicrobiomeWebsite/trunk@91353 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/R/shiny/apps/differential_abundance/server.R
+++ View/lib/R/shiny/apps/differential_abundance/server.R
@@ -108,12 +108,20 @@ shinyServer(function(input, output, session) {
       columns <<- new_columns
       
       if (is.null(properties)) {
-        mySelectedCategory <- column_factors
-        mySelectedFactor1 <- levels_factors[1]
-        mySelectedFactor2 <- levels_factors[2]
+        message("column_factors: ", column_factors)
+        mySelectedCategory <- NULL # column_factors
+        mySelectedFactor1 <- NULL #levels_factors[1]
+        mySelectedFactor2 <- NULL #levels_factors[2]
       } else {
         mySelectedCategory <- properties$selected[properties$input == "input$category"]
-        if (input$category != mySelectedCategory) {
+        dontUseProps <- FALSE
+        if (is.null(input$category)) {
+           dontUseProps <- TRUE 
+        } else if (input$category != mySelectedCategory) {
+           dontUseProps <- TRUE
+        }
+
+        if (dontUseProps) {
           mySelectedFactor1 <- levels_factors[1]
           mySelectedFactor2 <- levels_factors[2]
         } else {
@@ -126,7 +134,7 @@ shinyServer(function(input, output, session) {
       updateSelectizeInput(session, "category",
                            choices = c(columns[2:length(columns)]),
                            selected = mySelectedCategory,
-                           options = list(placeholder = 'Choose the metadata to calculate the differential abundance'),
+                           options = list(placeholder = 'Choose metadata to calculate differential abundance'),
                            server = T)
       
       
