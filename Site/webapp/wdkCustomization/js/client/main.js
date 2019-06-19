afac849d1d46193b14107418ec9a0c0eb4a88494
Dave Falke <dmfalke@gmail.com>
2019-06-19 18:17:45 +0000

    hide study and download link from study card, and make more prominent #35305
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/MicrobiomeWebsite/trunk@99114 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/main.js
+++ Site/webapp/wdkCustomization/js/client/main.js
@@ -1,9 +1,9 @@
-import 'site/css/MicrobiomeSite.css';
-
 import { initialize } from 'eupathdb/wdkCustomization/js/client/bootstrap';
 import componentWrappers from './componentWrappers';
 import wrapStoreModules from './wrapStoreModules';
 import { wrapRoutes } from './routes';
+
+import 'site/css/MicrobiomeSite.css';
 
 // Initialize the application.
 initialize({
