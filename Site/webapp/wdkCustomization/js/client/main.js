d221abd4a5be49a194191664d17879ce7dea0ee3
asizemore <annsize@seas.upenn.edu>
2022-02-15 17:48:56 -0500

    initialize access service

--- Site/webapp/wdkCustomization/js/client/main.js
+++ Site/webapp/wdkCustomization/js/client/main.js
@@ -1,16 +1,19 @@
 import { initialize } from '@veupathdb/web-common';
+import { partial } from 'lodash';
+import { edaServiceUrl } from '@veupathdb/web-common/lib/config';
 
 import '@veupathdb/web-common/lib/styles/client.scss';
 
 import componentWrappers from './componentWrappers';
 import wrapStoreModules from './wrapStoreModules';
 import { wrapRoutes } from './routes';
-
+import { wrapWdkDependencies } from '@veupathdb/study-data-access/lib/shared/wrapWdkDependencies';
 import 'site/css/MicrobiomeSite.css';
 
 // Initialize the application.
 initialize({
   componentWrappers,
+  wrapWdkDependencies: partial(wrapWdkDependencies, edaServiceUrl),
   wrapStoreModules,
   wrapRoutes,
 })
