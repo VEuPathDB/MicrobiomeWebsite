ca321082b1e98e6dd259475428c12363a6a93ab8
Dave Falke <dfalke@uga.edu>
2022-08-17 15:04:40 -0400

    Add site search route

--- Site/webapp/wdkCustomization/js/client/routes.jsx
+++ Site/webapp/wdkCustomization/js/client/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { useUserDatasetsWorkspace } from '@veupathdb/web-common/lib/config';
+import SiteSearchController from '@veupathdb/web-common/lib/controllers/SiteSearchController';
 
 import AboutController from './controllers/AboutController';
 import { userDatasetRoutes } from './routes/userDatasetRoutes';
@@ -11,7 +12,10 @@ export function wrapRoutes(ebrcRoutes) {
       path: '/about',
       component: () => <AboutController/>
     },
-    
+    {
+      path: '/search',
+      component: () => <SiteSearchController offerOrganismFilter={false} preferredOrganisms={false} />
+    },    
     ...(
       useUserDatasetsWorkspace
         ? userDatasetRoutes
