e0a254f2ef06ca08228d65fc98c9920481558d53
Dave Falke <dfalke@uga.edu>
2022-08-17 15:04:50 -0400

    Add local dev support for site search

--- Site/webpack.config.local.mjs
+++ Site/webpack.config.local.mjs
@@ -18,6 +18,7 @@ export default configure({
     rootClientUrl: process.env.ROOT_URL,
     proxies: {
       [process.env.WDK_SERVICE_ENDPOINT]: process.env.WDK_SERVICE_URL,
+      [process.env.SITE_SEARCH_SERVICE_ENDPOINT]: process.env.SITE_SEARCH_SERVICE_URL,
       [process.env.EDA_SERVICE_ENDPOINT]: process.env.EDA_SERVICE_URL,
       [process.env.USER_DATASETS_WORKSPACE_IMPORT_SERVICE_ENDPOINT]: process.env.USER_DATASETS_WORKSPACE_IMPORT_SERVICE_URL,
       [process.env.DOCUMENTS_ENDPOINT]: process.env.DOCUMENTS_URL,
@@ -37,6 +38,7 @@ export default configure({
         twitterUrl: process.env.TWITTER_URL,
         vimeoUrl: process.env.VIMEO_URL,
         communitySite: process.env.COMMUNITY_SITE,
+        siteSearchServiceUrl: process.env.SITE_SEARCH_SERVICE_ENDPOINT,
         ...(
           process.env.EDA_ENABLED === 'true'
             ? {
