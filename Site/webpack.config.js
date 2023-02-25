6f29ded72d1a22e73871ba83effd1d3f376529b6
Dave Falke <dfalke@uga.edu>
2023-02-25 00:12:05 -0500

    upgrade veupathdb packages

--- Site/webpack.config.js
+++ Site/webpack.config.js
@@ -3,7 +3,20 @@ var configure = require('@veupathdb/site-webpack-config');
 var additionalConfig = {
   entry: {
     'site-client': './webapp/wdkCustomization/js/client/main.js'
-  }
+  },
+  module: {
+    rules: [
+      // Apply babel to react-leaflet code.
+      // This can be removed when we upgrade to webpack@5.
+      {
+        test: /\.jsx?$/,
+        include: /node_modules\/@?react-leaflet/,
+        use: [
+          { loader: 'babel-loader', options: { configFile: './.babelrc' } }
+        ]
+      },
+    ],
+  },
 };
 
 module.exports = configure(additionalConfig);
