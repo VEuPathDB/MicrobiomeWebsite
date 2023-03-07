fe1ff3b95661d9c0a6e6a8b2850149d58780b025
aurreco-uga <aurreco@uga.edu>
2023-03-07 15:20:34 -0500

    mbiome UD details page: link to EDA
    (cherry picked from commit 52785893703aa60acf4589e9a801de4b8f0338b1)

--- Site/webapp/wdkCustomization/js/client/routes/userDatasetRoutes.tsx
+++ Site/webapp/wdkCustomization/js/client/routes/userDatasetRoutes.tsx
@@ -1,52 +1,82 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useMemo } from 'react';
+
+import { useLocation } from 'react-router-dom';
 
 import { Loading } from '@veupathdb/wdk-client/lib/Components';
 import { RouteEntry } from '@veupathdb/wdk-client/lib/Core/RouteEntry';
-import { useWdkService } from '@veupathdb/wdk-client/lib/Hooks/WdkServiceHook';
+
+import { makeEdaRoute } from '@veupathdb/web-common/lib/routes';
+import { diyUserDatasetIdToWdkRecordId } from '@veupathdb/web-common/lib/util/diyDatasets';
 
 import {
-  quotaSize
-} from '@veupathdb/user-datasets/lib/Components/UserDatasetUtils';
+  UserDatasetDetailProps
+} from '@veupathdb/user-datasets/lib/Controllers/UserDatasetDetailController';
+
 import {
   uploadTypeConfig
 } from '@veupathdb/user-datasets/lib/Utils/upload-config';
 
-const UserDatasetHelp = React.lazy(() => import('@veupathdb/user-datasets/lib/Components/UserDatasetHelp'));
+import { communitySite, projectId } from '@veupathdb/web-common/lib/config';
+
+import ExternalContentController from '@veupathdb/web-common/lib/controllers/ExternalContentController';
+
+const BiomDatasetDetail = React.lazy(
+  () => import('@veupathdb/user-datasets/lib/Components/Detail/BiomDatasetDetail')
+);
+
 const UserDatasetRouter = React.lazy(() => import('../controllers/UserDatasetRouter'));
 
-const availableUploadTypes = ['biom'];
+const availableUploadTypes = ['isasimple'];
+
+const USER_DATASETS_HELP_PAGE = `${projectId}/user_datasets_help.html`;
 
 export const userDatasetRoutes: RouteEntry[] = [
   {
     path: '/workspace/datasets',
     exact: false,
-    component: function MicrobiomeUserDatasetRouter() {
-      const projectName = useWdkService(
-        async wdkService => (await wdkService.getConfig()).displayName,
-        []
+    component: function ClinEpiUserDatasetRouter() {
+      const location = useLocation();
+
+      const helpTabContentUrl = useMemo(
+        () => [
+          communitySite,
+          USER_DATASETS_HELP_PAGE,
+          location.search,
+          location.hash
+        ].join(''),
+        [location.search, location.hash]
       );
+
+      const detailComponentsByTypeName = useMemo(() => ({
+        BIOM: function MbioBiomDatasetDetail(props: UserDatasetDetailProps) {
+          const wdkDatasetId = diyUserDatasetIdToWdkRecordId(props.userDataset.id);
+
+          return (
+            <BiomDatasetDetail
+              {...props}
+              edaWorkspaceUrl={`${makeEdaRoute(wdkDatasetId)}/new`}
+            />
+          )
+        }
+      }), []);
 
       return (
         <Suspense fallback={<Loading />}>
           <UserDatasetRouter
             availableUploadTypes={availableUploadTypes}
-            detailsPageTitle="My Data Set"
+            detailsPageTitle="My User Study"
             helpRoute="/workspace/datasets/help"
-            workspaceTitle="My Data Sets"
+            workspaceTitle="My User Studies"
             uploadTypeConfig={uploadTypeConfig}
+            detailComponentsByTypeName={detailComponentsByTypeName}
             helpTabContents={
-              projectName == null
-                ? <Loading />
-                : <UserDatasetHelp
-                    hasDirectUpload={availableUploadTypes.length > 0}
-                    projectName={projectName}
-                    quotaSize={quotaSize}
-                    workspaceTitle="My Data Sets"
-                  />
+              <ExternalContentController
+                url={helpTabContentUrl}
+              />
             }
           />
         </Suspense>
       );
-    }
+    },
   }
 ];
