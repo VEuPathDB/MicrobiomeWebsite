93a2c3f9253dd70d5da774969502344f92273e37
Dave Falke <dfalke@uga.edu>
2023-03-29 11:49:29 -0400

    Use monorepo assets and remove code that has been moved

--- Site/webapp/wdkCustomization/js/client/controllers/AboutController.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { PageController } from '@veupathdb/wdk-client/lib/Controllers';
-import About from '../components/About';
-
-export default class AboutController extends PageController {
-  renderView() {
-    return <About/>
-  }
-}
